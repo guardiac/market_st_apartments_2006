4f8a299ec76f31af6095714c8239c8e602c86e1a
Angela Guardia <angelaguardia20@gmail.com>
2020-07-14 08:50:37 -0700

    Add test_it_can_add_units test to building test class

--- test/building_test.rb
+++ test/building_test.rb
@@ -17,6 +17,14 @@ class BuildingTest < Minitest::Test
     assert_equal [], @building.units
   end
 
+  def test_it_can_add_units
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    @building.add_unit(unit1)
+
+    assert_equal [unit1], @building.units
+  end
+
   def test_it_has_renters
     assert_equal [], @building.renters
   end
