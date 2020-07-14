0662cfa6aa82d47e3f7f356b2221ea68bd53b980
Angela Guardia <angelaguardia20@gmail.com>
2020-07-14 09:15:24 -0700

    Refator renter method in building class

--- lib/building.rb
+++ lib/building.rb
@@ -1,13 +1,17 @@
 class Building
-  attr_reader :units,
-              :renters
+  attr_reader :units
 
   def initialize
     @units = []
-    @renters = []
   end
 
   def add_unit(unit)
     @units << unit
+  end
+
+  def renters
+    @units.map do |unit|
+      unit.renter.name if unit.renter
+    end.compact
   end
 end
