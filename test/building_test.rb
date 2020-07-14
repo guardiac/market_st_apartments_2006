89c6b82e15b719a7c6cdd3b57274b4d60b893f09
Angela Guardia <angelaguardia20@gmail.com>
2020-07-14 09:15:05 -0700

    Refactor building renters method tests

--- test/building_test.rb
+++ test/building_test.rb
@@ -25,7 +25,29 @@ class BuildingTest < Minitest::Test
     assert_equal [unit1], @building.units
   end
 
-  def test_it_has_renters
+  def test_it_has_no_renters_if_units_empty
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    @building.add_unit(unit1)
+
     assert_equal [], @building.renters
+  end
+
+  def test_it_has_renters_if_units_occupied
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Aurora")
+
+    unit1.add_renter(renter1)
+    @building.add_unit(unit1)
+
+    assert_equal ["Aurora"], @building.renters
+
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    renter2 = Renter.new("Tim")
+
+    unit2.add_renter(renter2)
+    @building.add_unit(unit2)
+
+    assert_equal ["Aurora", "Tim"], @building.renters
   end
 end
